f5e032bc7d0b97d8127f2d6e048d3c0c721bcd90
Mal F <malfellows13@gmail.com>
2022-09-15 22:03:48 -0500

    eat_lunch() function done

--- functions_practice.py
+++ functions_practice.py
@@ -10,6 +10,7 @@ hello()
 # 2
 # A function named pack() that accepts three arguments, 
 #and returns a single list with the three arguments inside as list elements.
+
 def pack(item1, item2, item3):
     return [item1, item2, item3]
 
@@ -22,3 +23,22 @@ print("The items packed are:", pack("apple", "sandwich", "cookie"))
 # (the first element of the list), and "Next I eat ___" 
 # (for the following elements in the list). 
 # If the list is empty, print "My lunchbox is empty!". The function should not return anything.
+
+def eat_lunch(list):
+    # use len() to check the number of elements in the list
+    # if len() is 0, then lunchbox is empty. If not empty, move to else.
+    if len(list) == 0:
+        print("My lunchbox is empty!")
+    else:
+        # if there is food here, the first prints as "first" automatically
+        print("First I eat", list[0])
+        # then switch to range for the rest
+        # Range begins at 1 (so the list item after the first one)
+        # Range continues for length of list
+        for item in range(1, len(list)):
+            print("Next I eat", list[item])
+
+my_list = ["apple", "sandwich", "cookie"]
+my_list2 = []
+eat_lunch(my_list)
+eat_lunch(my_list2)
