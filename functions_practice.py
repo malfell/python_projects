09b135b0050d28b8f76264180263be3597ebbeee
Mal F <malfellows13@gmail.com>
2022-09-15 21:29:51 -0500

    first commit

---
+++ functions_practice.py
@@ -0,0 +1,17 @@
+# 1
+# A function named hello() that prints a greeting to the user. 
+#This function should accept no arguments and returns nothing.
+
+
+# 2
+# A function named pack() that accepts three arguments, 
+#and returns a single list with the three arguments inside as list elements.
+
+
+# 3
+# A function called eat_lunch(). 
+# This function should accept a list of any length, 
+# and print out a series of strings that say "First I eat __" 
+# (the first element of the list), and "Next I eat ___" 
+# (for the following elements in the list). 
+# If the list is empty, print "My lunchbox is empty!". The function should not return anything.
